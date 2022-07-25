c328081579f3ebd9ab8ede7dce8c610107c48de0
ErenZ0 <er3nkozak@gmail.com>
2022-07-25 10:44:31 +0300

    Create Switch-CaseYapısı.cs

---
+++ Switch-CaseYapısı.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace program;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch(month)
+            {
+                case 1:
+                Console.WriteLine("ocak ayındasınız");
+                break;
+
+                case 2:
+                Console.WriteLine("şubat ayındasınız");
+                break;
+
+                case 4:
+                Console.WriteLine("nisan ayındasınız");
+                break;
+
+                case 3:
+                Console.WriteLine("mart ayındasınız");
+                break;
+
+                default : 
+                Console.WriteLine("yanlış veri girişi");
+                break;
+
+                
+
+
+            }
+
+        }
+    }
